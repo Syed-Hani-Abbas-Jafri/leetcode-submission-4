2fc74298eeefbbe1778eee4efade1ab9863cbdca
Sayyid Hani Abbas Ja'fari <k250085@nu.edu.pk>
2025-12-03 10:04:24 +0500

    Implement climbStairs function for stair climbing

---
+++ recursion1.c
@@ -0,0 +1,17 @@
+int climbStairs(int n) {
+    if (n == 1) return 1;
+    if (n == 2) return 2;
+     int a = 1;
+     int b = 2;
+    int current;
+
+    for (int i=3; i<=n; i++) {
+        current = a + b;
+        a = b;
+        b = current;
+    }
+
+    return current;
+
+
+}
